8b9af4de5f09b5a098067b02c0e70d422f76736f
Harshal Arun Patil <harshalapatil2796@gmail.com>
2022-05-13 15:11:37 +0530

    [Harshal]Add:Using MSTesting CoffeeMaking Demo Done

---
+++ CoffeeMaker/StarBucks..cs
@@ -0,0 +1,38 @@
+﻿namespace CoffeeMaker
+{
+    public class StarBucks : IMakeACoffee //moq
+    {
+        public bool CheckIngridientAvailability()
+        {
+            return true;
+        }
+        public string CoffeeMaking(int sugerPerSpoon, int coffeePack)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class StubStarBucks : IMakeACoffee //stub
+    {
+        public bool CheckIngridientAvailability()
+        {
+            return true;
+        }
+
+        public string CoffeeMaking(int sugerPerSpoon, int coffeePack)
+        {
+            return "Your Order Is Received";
+        }
+    }
+    public class FakeStarBucks : IMakeACoffee //fake
+    {
+        public bool CheckIngridientAvailability()
+        {
+            return true;
+        }
+
+        public string CoffeeMaking(int sugerPerSpoon, int coffeePack)
+        {
+            return "Your Order Is Received";
+        }
+    }
+}
